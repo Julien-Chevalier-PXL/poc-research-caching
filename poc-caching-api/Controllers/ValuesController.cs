9374893f946240e625529648e3e64eab0507b455
Julien Chevalier <julien.a.chevalier@capgemini.com>
2018-05-30 17:00:47 +0200

    [INIT] Init net core project

---
+++ poc-caching-api/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿namespace poc_caching_api.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("poc-caching-api/logs")]
+    public class ValuesController : Controller
+    {
+        // GET poc-caching-api/logs/index
+        [HttpGet("{index}")]
+        public IEnumerable<string> Get(string index)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET poc-caching-api/logs/id
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
