639cadbc6cf6cac5c0fd11c2a89beffb7f79b713
Julien-Chevalier-PXL <11500546@student.pxl.be>
2018-06-11 00:41:18 +0200

    [RedisLayer] CalculationResponse class

---
+++ poc-caching-api/BusinessLayer/RedisLayer/Models/CalculationResponse.cs
@@ -0,0 +1,8 @@
+namespace pocCachingApi.BusinessLayer.RedisLayer.Models
+{
+    public class CalculationResponse
+    {
+        public long Sum { get; set; }
+        public double Average { get; set; }
+    }
+}
