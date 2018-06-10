774728c6cd9563fe3703a7f782953972b55c28fb
Julien-Chevalier-PXL <11500546@student.pxl.be>
2018-06-11 00:43:02 +0200

    [API] CalculationController

---
+++ poc-caching-api/Controllers/CalculationController.cs
@@ -0,0 +1,43 @@
+﻿namespace pocCachingApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using pocCachingApi.BusinessLayer;
+
+    [Route("poc-caching-api/calculation")]
+    public class CalculationController : Controller
+    {
+        // GET poc-caching-api/calculation
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var service = new CalculatorCachingService();
+            var result = await service.GetResults(id);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"Process time: {elapsedMs.ToString()} ms");
+            Console.WriteLine("----------------------------------------------");
+            
+            if (result.isValid)
+            {
+                if (result.Result != null)
+                {
+                    return Ok(result.Result);
+                }
+                else
+                {
+                    return Ok($"{result.Message}");
+                }
+            }
+
+            return StatusCode(500, result.Message);
+        }
+    }
+}
